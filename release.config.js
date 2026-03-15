bb82e801fe9884f43c8dd2c628dbd51c885d4f7f
Danil Trapeznikov <danil@iglu.net>
2026-03-15 12:18:30 +0700

    chore(release): update GitHub Actions workflow for release process and add permissions for better access control chore(release): switch from yarn to npm for package management in the release workflow chore(release): change branch name from main to master in release configuration chore(release): add changelog generation and git commit for release assets in release configuration

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  branches: ['main'],
+  branches: ['master'],
   plugins: [
     [
       '@semantic-release/commit-analyzer',
@@ -12,7 +12,20 @@ module.exports = {
       },
     ],
     '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
     '@semantic-release/npm',
     '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
   ],
 };
