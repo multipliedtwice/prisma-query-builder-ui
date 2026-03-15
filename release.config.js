269095ba9b98673eee15fd700973ed61441e7f5e
Danil Trapeznikov <danil@iglu.net>
2026-03-15 12:21:07 +0700

    chore(release): update GitHub Actions workflow to use yarn instead of npm for package management chore(release): remove changelog generation and git commit steps from semantic release configuration

--- release.config.js
+++ release.config.js
@@ -12,20 +12,7 @@ module.exports = {
       },
     ],
     '@semantic-release/release-notes-generator',
-    [
-      '@semantic-release/changelog',
-      {
-        changelogFile: 'CHANGELOG.md',
-      },
-    ],
     '@semantic-release/npm',
     '@semantic-release/github',
-    [
-      '@semantic-release/git',
-      {
-        assets: ['CHANGELOG.md', 'package.json'],
-        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-      },
-    ],
   ],
 };
